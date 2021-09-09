87cc9d3d9591ae0bb6a502944f5c880f2f2d2b6a
한현규 <siregy1222@gmail.com>
2021-09-09 21:45:05 +0900

    Apply authorization

---
+++ src/main/java/com/kideveloper/my_spring_web/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kideveloper.my_spring_web.repository;
+
+import com.kideveloper.my_spring_web.model.Board;
+import com.kideveloper.my_spring_web.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
