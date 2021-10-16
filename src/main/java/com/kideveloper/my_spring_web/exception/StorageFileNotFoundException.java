07eebc1fae5f50bae5d3b578e76a6c1042a47e76
한현규 <siregy1222@gmail.com>
2021-10-16 21:42:54 +0900

    Spring File upload

---
+++ src/main/java/com/kideveloper/my_spring_web/exception/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package com.kideveloper.my_spring_web.exception;
+
+public class StorageFileNotFoundException extends StorageException{
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
