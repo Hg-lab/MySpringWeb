fbd73abb957831d272d03e055e36e53a4cfffe3a
한현규 <siregy1222@gmail.com>
2022-06-06 22:21:13 +0900

    Configuring call API Controller and Service Isolation

--- src/main/java/com/kideveloper/my_spring_web/controller/CallAPIController.java
+++ src/main/java/com/kideveloper/my_spring_web/controller/CallAPIController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kideveloper.my_spring_web.repository.CommonCodeRepository;
+import com.kideveloper.my_spring_web.service.CallCompanyJsonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -29,6 +30,9 @@ public class CallAPIController {
     @Autowired
     private CommonCodeRepository repository;
 
+    @Autowired
+    private CallCompanyJsonService callCompanyJsonService;
+
     @GetMapping("/")
     public String redirectCallApi() {
         return "callapi/identity";
@@ -36,63 +40,20 @@ public class CallAPIController {
 
     @GetMapping("/identity")
     public String identity(Model model, @RequestParam(required = false) String corp_code) throws JsonProcessingException {
-
-        HashMap<String, Object> result = new HashMap<String, Object>();
-
-        String jsonInString = "";
-
+        HashMap<String, Object> result = new HashMap<String, Object>(callCompanyJsonService.callCompanyJson(corp_code));
         try {
 
-            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-            factory.setConnectTimeout(5000); //타임아웃 설정 5초
-            factory.setReadTimeout(5000);//타임아웃 설정 5초
-            RestTemplate restTemplate = new RestTemplate(factory);
-
-            HttpHeaders header = new HttpHeaders();
-            HttpEntity<?> entity = new HttpEntity<>(header);
-
-            String url = "https://opendart.fss.or.kr/api/company.json";
-
-            String crtfc_key = repository.findFirstByComCd("crtfc_key_dart_api").getComCdVal();
-
-            UriComponents uri = UriComponentsBuilder.fromHttpUrl(
-                    url+"?crtfc_key="+crtfc_key+"&corp_code="+corp_code).build();
-
-            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
-            ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
-
-
-            // resultMap -> HashMap<String, Object> result 의 body에 put
-            result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
-            result.put("header", resultMap.getHeaders()); //헤더 정보 확인
-            result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
-
-            //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
-            ObjectMapper mapper = new ObjectMapper();
-            jsonInString = mapper.writeValueAsString(resultMap.getBody());
-
-            Map<String,Object> jsonMap = mapper.readValue(jsonInString, new TypeReference<Map<String,Object>>(){});
-
-            // model에는 map을 통채로 attribute로 넣으면 되어서 이부분 삭제
-//            Set<String> keySet = jsonMap.keySet();
-//            for(String key : keySet) {
-//                model.addAttribute(key, jsonMap.get(key));
-//            }
-
-            model.addAttribute("jsonMap", jsonMap);
+            model.addAttribute("jsonMap", result);
 
         } catch (HttpClientErrorException | HttpServerErrorException e) {
             result.put("statusCode", e.getRawStatusCode());
             result.put("body"  , e.getStatusText());
-            System.out.println("dfdfdfdf");
             System.out.println(e.toString());
-
         } catch (Exception e) {
             result.put("statusCode", "999");
             result.put("body"  , "excpetion오류");
             System.out.println(e.toString());
         }
-
         return "callapi/identity";
     }
 
