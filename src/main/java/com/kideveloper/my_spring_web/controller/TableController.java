c59245a8fccb7d7b8df0309ac7cc226325d3bbf6
한현규 <siregy1222@gmail.com>
2022-07-28 21:49:46 +0900

    Modify home controller for new index

--- src/main/java/com/kideveloper/my_spring_web/controller/TableController.java
+++ src/main/java/com/kideveloper/my_spring_web/controller/TableController.java
@@ -1,18 +1,16 @@
 package com.kideveloper.my_spring_web.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+@RequestMapping("tables")
+public class TableController {
 
     @GetMapping
-    public String index() {
-        return "index";
-    }
-
-    @GetMapping("/index2")
-    public String index1() {
-        return "index2";
+    public String tables() {
+        return "tables/tables";
     }
 }
