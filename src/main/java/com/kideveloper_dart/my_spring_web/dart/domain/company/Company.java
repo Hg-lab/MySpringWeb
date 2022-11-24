b6b3b6e172f717ffc2df9dfceda121bd2cea6cf0
Hg-lab <siregy1222@gmail.com>
2022-11-25 00:57:42 +0900

    feat: company entity

--- src/main/java/com/kideveloper_dart/my_spring_web/dart/domain/company/Company.java
+++ src/main/java/com/kideveloper_dart/my_spring_web/dart/domain/company/Company.java
@@ -1,6 +1,25 @@
 package com.kideveloper_dart.my_spring_web.dart.domain.company;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Company {
 
     // stock_code로 조회
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String corpCode;
+
+    @Column
+    private String name;
+
+    @Column
+    private String stockCode;
+
+    @Column
+    private Date modifyDate;
 }
