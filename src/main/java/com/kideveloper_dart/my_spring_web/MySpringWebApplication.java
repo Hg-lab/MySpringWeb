09b5570b0f4f596376e4eecb9d7d73aed9e50cd7
Hg-lab <siregy1222@gmail.com>
2022-11-28 17:56:53 +0900

    Enable Jpa Auditing

--- src/main/java/com/kideveloper_dart/my_spring_web/MySpringWebApplication.java
+++ src/main/java/com/kideveloper_dart/my_spring_web/MySpringWebApplication.java
@@ -2,7 +2,9 @@ package com.kideveloper_dart.my_spring_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MySpringWebApplication {
 
