6df1f30e1496a9f89d3115b4f7286812c3e0af93
Hg-lab <siregy1222@gmail.com>
2022-12-05 19:47:24 +0900

    feat: Assemble table by cell, row, column dto

--- src/main/java/com/kideveloper_dart/my_spring_web/dart/application/dto/response/DartDocsResponseDTO.java
+++ src/main/java/com/kideveloper_dart/my_spring_web/dart/application/dto/response/DartDocsResponseDTO.java
@@ -1,20 +1,22 @@
 package com.kideveloper_dart.my_spring_web.dart.application.dto.response;
 
+import com.kideveloper_dart.my_spring_web.dart.domain.cell.Cell;
+import com.kideveloper_dart.my_spring_web.dart.domain.column.ColumnHead;
+import com.kideveloper_dart.my_spring_web.dart.domain.row.RowHead;
 import lombok.*;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 @Getter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DartDocsResponseDTO {
 
-    private List<CellDTO> cellDTOList;
+    private final List<ColumnHeadDTO> columnHeadDTOList = new ArrayList<>();
+    private final LinkedHashMap<RowHeadDTO, Map<ColumnHeadDTO, CellDTO>> rowColumnCellDTOMap = new LinkedHashMap<>();
 
-    private LinkedHashMap<RowDTO, HashMap<ColumnDTO, CellDTO>> rowColumnCellDTOMap;
+    public DartDocsResponseDTO() {
+
+    }
 
     // TODO: 2022/12/01 Entity -> DTO, Entity 변동발생시 API 스펙을 보존하기위함
     @Builder
@@ -22,19 +24,44 @@ public class DartDocsResponseDTO {
     public static class CellDTO {
         private String value;
         private Integer term;
+
+        public static CellDTO from(Cell cell) {
+            return CellDTO.builder()
+                    .value(cell.getValue())
+                    .term(cell.getTerm())
+                    .build();
+        }
     }
 
     @Builder
     @Data
-    public static class RowDTO {
+    public static class RowHeadDTO {
         private String name;
         private Integer term;
         private Integer rowOrder;
+
+        public static RowHeadDTO from(RowHead rowHead) {
+            return RowHeadDTO.builder()
+                    .name(rowHead.getName())
+                    .term(rowHead.getTerm())
+                    .rowOrder(rowHead.getRowOrder())
+                    .build();
+        }
     }
 
     @Builder
     @Data
-    public static class ColumnDTO {
+    public static class ColumnHeadDTO {
         private String name;
+        private Integer term;
+        private Integer columnOrder;
+
+        public static ColumnHeadDTO from(ColumnHead columnHead) {
+            return ColumnHeadDTO.builder()
+                    .name(columnHead.getName())
+                    .term(columnHead.getTerm())
+                    .columnOrder(columnHead.getColumnOrder())
+                    .build();
+        }
     }
 }
--- src/main/java/com/kideveloper_dart/my_spring_web/dart/domain/TableAssembler.java
+++ src/main/java/com/kideveloper_dart/my_spring_web/dart/domain/TableAssembler.java
@@ -1,4 +1,35 @@
 package com.kideveloper_dart.my_spring_web.dart.domain;
 
+
+import com.kideveloper_dart.my_spring_web.dart.application.dto.response.DartDocsResponseDTO;
+import com.kideveloper_dart.my_spring_web.dart.application.dto.response.DartDocsResponseDTO.ColumnHeadDTO;
+import com.kideveloper_dart.my_spring_web.dart.domain.cell.Cell;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.kideveloper_dart.my_spring_web.dart.application.dto.response.DartDocsResponseDTO.*;
+
+@Component
 public class TableAssembler {
+
+
+    public DartDocsResponseDTO assembleTableByCells(List<Cell> cells) {
+        DartDocsResponseDTO dartDocsResponseDTO = new DartDocsResponseDTO();
+
+        for (Cell cell : cells) {
+            ColumnHeadDTO columnHeadDTO = ColumnHeadDTO.from(cell.getColumnHead());
+            RowHeadDTO rowHeadDTO = RowHeadDTO.from(cell.getRowHead());
+
+            dartDocsResponseDTO.getColumnHeadDTOList().add(columnHeadDTO);
+
+            Map<ColumnHeadDTO, CellDTO> columnCellMapDTO = new HashMap<>();
+            columnCellMapDTO.put(columnHeadDTO, CellDTO.from(cell));
+            dartDocsResponseDTO.getRowColumnCellDTOMap().put(rowHeadDTO, columnCellMapDTO);
+        }
+        return dartDocsResponseDTO;
+    }
+
 }
