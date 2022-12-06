4ab3bff1b68f36d22a067da7cbda12e4daef5271
Hg-lab <siregy1222@gmail.com>
2022-12-06 20:35:17 +0900

    feat: repository

--- src/main/java/com/kideveloper_dart/my_spring_web/dart/domain/repository/CellRepository.java
+++ src/main/java/com/kideveloper_dart/my_spring_web/dart/domain/repository/CellRepository.java
@@ -1,9 +1,13 @@
 package com.kideveloper_dart.my_spring_web.dart.domain.repository;
 
 import com.kideveloper_dart.my_spring_web.dart.domain.cell.Cell;
+import com.kideveloper_dart.my_spring_web.dart.domain.documentation.Documentation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CellRepository extends JpaRepository<Cell, Long> {
+    List<Cell> findByDocumentation(Documentation documentation);
 }
--- src/main/java/com/kideveloper_dart/my_spring_web/dart/domain/repository/DocumentationRepository.java
+++ src/main/java/com/kideveloper_dart/my_spring_web/dart/domain/repository/DocumentationRepository.java
@@ -14,4 +14,6 @@ public interface DocumentationRepository extends JpaRepository<Documentation, Lo
     List<Documentation> findByCompany(Company company);
 
     boolean existsByCompanyAndBusinessYear(Company company, Integer businessYear);
+
+    Documentation findByCompanyAndAndBusinessYear(Company company, Integer businessYear);
 }
